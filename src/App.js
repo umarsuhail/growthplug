a13d9bb9061e533d7ba06289bd8c30554fc49179
uvionics <umar.suhail@uvionicstech.com>
2019-10-22 12:23:37 +0530

    footer

--- src/App.js
+++ src/App.js
@@ -5,16 +5,45 @@ import Navbar from './components/Navbar'
 import SimpleSlider from './components/carousel'
 import Card from './components/Card'
 import Footer from "./components/Footer";
+import BlogUpdate from './components/Blogupdate'
+import blog1 from './images/new1.png'
+import blog3 from './images/blog3.png'
+import blog2 from './images/blog2.png'
+
+
 function App() {
   return (
     <div className="App">
-    <Navbar></Navbar>
-    <div style={{marginTop:'60px'}}>
-    <SimpleSlider/>
-    <Card></Card>
-    <Footer></Footer>
-    </div>
-   
+      <Navbar></Navbar>
+      <div style={{ marginTop: '60px' }}>
+        <SimpleSlider />
+        <Card></Card>
+        <Footer></Footer>
+        <div className="container" style={{marginTop:80}}>
+        <div className="float-left" style={{marginBottom:50}}>
+            <h3>Blog Update</h3>
+          </div>
+          <div className="float-right">
+            <button className="btn btn-primary view">View All</button>
+          </div>
+          <div className="container row" style={{
+          margin: '0 auto',
+          justifyContent: 'center'
+        }}>
+
+          <BlogUpdate image={blog3} />
+          <BlogUpdate image={blog2} />
+          <BlogUpdate image={blog1} />
+          <BlogUpdate image={blog2} />
+          <BlogUpdate image={blog1} />
+          <BlogUpdate image={blog3} />
+    
+
+        </div>
+        </div>
+
+      </div>
+
     </div>
   );
 }
---
+++ src/components/Blogupdate.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+export default class Blogupdate extends Component {
+  render() {
+    return (
+        <div class="card column" style={{width:'18rem'}}>
+          <img src={this.props.image} class="card-img-top" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title" style={{color:'orange'}}>Blog Title goes here..</h5>
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+          </div>
+        </div>
+    );
+  }
+}
